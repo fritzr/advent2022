804c7d01410e42f41ff9303f7586d04eae4f0929
Fritz Reese <fritzoreese@gmail.com>
2022-12-04 08:46:18 -0400

    Day 4 part 2

--- src/advent2022/day04/__init__.py
+++ src/advent2022/day04/__init__.py
@@ -3,8 +3,11 @@ def contains(range1, range2):
     assert(range1[0] <= range1[1] and range2[0] <= range2[1])
     return range1[0] >= range2[0] and range1[1] <= range2[1]
 
+def overlaps(range1, range2):
+    return not (range2[1] < range1[0] or range2[0] > range1[1])
+
 def main(stream, opts):
-    containments = 0
+    containments = overlappings = 0
     for line in stream:
         elves = line.strip().split(",")
         assert(len(elves) == 2)
@@ -13,9 +16,14 @@ def main(stream, opts):
             for room_range in elves
         ]
         for index in range(len(rooms)):
-            if contains(rooms[index], rooms[index-1]):
-                containments += 1
+            if overlaps(rooms[index], rooms[index-1]):
+                overlappings += 1
+                if (
+                        contains(rooms[index], rooms[index-1])
+                        or contains(rooms[index-1], rooms[index])
+                    ):
+                    containments += 1
                 break
 
-
     print("part 1:", containments)
+    print("part 2:", overlappings)
