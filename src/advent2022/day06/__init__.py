83c6c4978bd652663f953115bb4ed4a242dfa1bd
Fritz Reese <fritzoreese@gmail.com>
2022-12-07 01:38:57 -0500

    More efficient day 6

--- src/advent2022/day06/__init__.py
+++ src/advent2022/day06/__init__.py
@@ -1,15 +1,27 @@
 from collections import deque
 
 def find_marker(string, marker_len):
-    buffer = deque(string[:marker_len])
+    nunique = 0
+    nelem = 0
+    bufset = [0] * 26 # a - z
+    char = None
     for index in range(marker_len, len(string)):
-        buffer.popleft()
-        buffer.append(string[index])
-        if len(set(buffer)) == len(buffer):
+        if nelem == marker_len:
+            oldchar = ord(string[index-marker_len]) - ord('a')
+            bufset[oldchar] -= 1
+            nelem -= 1
+            if bufset[oldchar] == 0:
+                nunique -= 1
+        char = ord(string[index]) - ord('a')
+        if not bufset[char]:
+            nunique += 1
+        bufset[char] += 1
+        nelem += 1
+        if nelem == marker_len and nunique == marker_len:
             return index
     return -1
 
 def main(stream, opts):
-    string = stream.read()
+    string = stream.read().strip()
     print("part 1:", find_marker(string, 4) + 1)
     print("part 2:", find_marker(string, 14) + 1)
