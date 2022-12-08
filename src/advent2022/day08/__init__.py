733a7534d120d577445f496a8b024f6d35655b0d
Fritz Reese <fritzoreese@gmail.com>
2022-12-08 00:45:20 -0500

    Day 8

---
+++ src/advent2022/day08/__init__.py
@@ -0,0 +1,41 @@
+from operator import mul
+from functools import reduce
+
+def scenic_score(forest, row, col, opts):
+    score = 1
+    viewable = False
+    dir_scores = [0, 0, 0, 0]
+    for dir_id, colrange in enumerate((range(col-1, -1, -1), range(col+1, len(forest[row])))):
+        for ocol in colrange:
+            dir_scores[dir_id] += 1
+            if forest[row][ocol] >= forest[row][col]:
+                break
+        else:
+            viewable = True
+    for dir_id, rowrange in enumerate((range(row-1, -1, -1), range(row+1, len(forest)))):
+        for orow in rowrange:
+            dir_scores[dir_id + 2] += 1
+            if forest[orow][col] >= forest[row][col]:
+                break
+        else:
+            viewable = True
+    return viewable, reduce(mul, dir_scores)
+
+def main(stream, opts):
+    forest = [line[:-1] for line in stream.readlines()]
+
+    nvisible = 0
+    best_view = 0
+    best_loc = (-1, -1)
+    for row in range(len(forest)):
+        for col in range(len(forest[0])):
+            viewable, score = scenic_score(forest, row, col, opts)
+            if viewable:
+                nvisible += 1
+            if score > best_view:
+                best_view = score
+                best_loc = (row, col)
+
+    print("part 1:", nvisible)
+    print("part 2:", best_view, "from", best_loc, "=", forest[best_loc[0]][best_loc[1]])
+
